7ae9b93e0bd2942547b16f4fe2b8e28fe9a90d64
Tanvir Islam streame <streametanvir@gmail.com>
2020-08-23 08:21:04 +0600

    init

---
+++ precache-manifest.eece081bac81c84d34311dab10f12220.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c14a778709be2e2ec8729b2de72d0fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8012571fbaaac04b7ac",
+    "url": "/static/css/2.68a684dc.chunk.css"
+  },
+  {
+    "revision": "d1434710441c9e166f63",
+    "url": "/static/css/main.437d7f6f.chunk.css"
+  },
+  {
+    "revision": "c8012571fbaaac04b7ac",
+    "url": "/static/js/2.911a4637.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.911a4637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1434710441c9e166f63",
+    "url": "/static/js/main.ac125f0f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1a2a757b757b291fe733d6d629b6f2ee",
+    "url": "/static/media/cart.1a2a757b.svg"
+  },
+  {
+    "revision": "900617fa1384306b48b2107700046f9b",
+    "url": "/static/media/copyright-claim.900617fa.png"
+  },
+  {
+    "revision": "5e147cf86cad62618ab28c518166d576",
+    "url": "/static/media/course-preview-thumbnail.5e147cf8.png"
+  },
+  {
+    "revision": "c27b00bd812513b4a8350bc6228032b8",
+    "url": "/static/media/ecommerce-project.c27b00bd.png"
+  },
+  {
+    "revision": "2c6833cd9c9761f55510de5cb97a5080",
+    "url": "/static/media/ellipse-red.2c6833cd.png"
+  },
+  {
+    "revision": "7ec8d06923a113ca35d2a876078f8fd7",
+    "url": "/static/media/frequently-asked-bg.7ec8d069.png"
+  },
+  {
+    "revision": "f83b1f3358f4e1b4e0ca21edfab6b027",
+    "url": "/static/media/full-stack.f83b1f33.png"
+  },
+  {
+    "revision": "ee2b82a6e8bec57a1aab5873816ff59e",
+    "url": "/static/media/hero-bg.ee2b82a6.png"
+  },
+  {
+    "revision": "5fef0da90164b21dc64e58a6bb19104f",
+    "url": "/static/media/jhankar.5fef0da9.png"
+  },
+  {
+    "revision": "482e93739c3b1d57925279482db87177",
+    "url": "/static/media/login-bg.482e9373.png"
+  },
+  {
+    "revision": "4504d35a55d51f6beaeca782be69c54c",
+    "url": "/static/media/man-with-laptop.4504d35a.png"
+  },
+  {
+    "revision": "597b129320200cb8656909e6a61e07b7",
+    "url": "/static/media/panda-project.597b1293.png"
+  },
+  {
+    "revision": "44e28ccdc1ffda07f54a4675d057cc56",
+    "url": "/static/media/pc-icon.44e28ccd.png"
+  },
+  {
+    "revision": "c173a3056d29db04013cce0667a509d2",
+    "url": "/static/media/protfolio-project.c173a305.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "1693ee8e3a6131a8143b92234751c1cc",
+    "url": "/static/media/thinker.1693ee8e.png"
+  },
+  {
+    "revision": "f74860a78ccb2306c064fb82f5f01744",
+    "url": "/static/media/winner-trophy.f74860a7.svg"
+  },
+  {
+    "revision": "51378a3969592f318f91c0f09f010f4d",
+    "url": "/static/media/work-time-pana.51378a39.svg"
+  }
+]);
